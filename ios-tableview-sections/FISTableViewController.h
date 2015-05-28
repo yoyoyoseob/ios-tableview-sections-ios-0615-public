efe321f05950f7b84f1313fcd6d762e0f5307c62
TOMalley104 <tom@flatironschool.com>
2015-05-28 15:32:50 -0400

    initial commit, barebones lab for students, needs README

---
+++ ios-tableview-sections/FISTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISTableViewController.h
+//  ios-tableview-sections
+//
+//  Created by Tom OMalley on 5/28/15.
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISTableViewController : UITableViewController
+
+@end
